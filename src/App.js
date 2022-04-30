2856504d07c370c4b4f00983a85030a63ee3e235
Vinícius Rovigo Medeiros <viniciusrovigomedeiros@MacBook-Air-de-Vinicius.local>
2022-04-30 10:02:51 -0300

    Projeto carrosel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const carousel = useRef(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/static/list1.json')
+    .then((response) => response.json())
+    .then(setData);
+  }, [])
+
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  }
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  }
+
+  if (!data || !data.length) return null;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='container'>
+      <h1 className='h1'>Lançamentos</h1>
+            <div className='carousel' ref={carousel}>
+              {data.map((item) => {
+                const {id, image} = item;
+                return (
+                  <div className='item' key={id}>
+                    <div className='image'>
+                        <img src= {image} />
+                    </div>
+                </div>
+                );
+                })}
+            </div>
+{/* botao */}
+            <div className='buttons'>
+                <button onClick={handleLeftClick}>
+                  <img src='/static/images/216151_right_chevron_icon.png' alt='Left'/>
+                  </button>
+                <button onClick={handleRightClick}>
+                  <img src='/static/images/216151_right_chevron_icon.png' alt='Right'/>
+                  </button>
+            </div>
+{/* botao */}
+
+        </div>
+        
+    );
+
+    
 }
+
+
+
 
 export default App;
